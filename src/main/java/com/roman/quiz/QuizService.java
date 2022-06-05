467a07aa5a9887e458d6e70d64b16f88d82c875f
roman <romul1nrome@gmail.com>
2022-06-05 08:24:55 +0300

    Remove commented code

--- src/main/java/com/roman/quiz/QuizService.java
+++ src/main/java/com/roman/quiz/QuizService.java
@@ -4,7 +4,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
 
 @Component
 public class QuizService {
